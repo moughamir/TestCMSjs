9b8e1d0c5c72f2801cc9e2a63f5d2f32eb78e7cb
Mohamed Moughamir <moughamir@gmail.com>
2016-01-31 19:43:02 +0000

    Changing sitename to logo

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ $(function() {
   CMS.init({
 
     // Name of your site or location of logo file, relative to root directory (img/logo.png)
-    siteName: 'Moughamir',
+    siteName: 'img/logo.svg',
 
     // Tagline for your site
     siteTagline: 'Tada',
